002f1e5e1dd06b9b6ae4765b6d39733773cad6d7
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2025-02-24 17:04:09 -0300

    First Commit

---
+++ src/test/java/com/example/CouseAPI/CouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CouseAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
