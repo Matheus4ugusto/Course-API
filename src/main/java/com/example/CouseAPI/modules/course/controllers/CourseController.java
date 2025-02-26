58efb74e7b7205010975dee7dff0225c7aa09624
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2025-02-26 16:37:42 -0300

    Cria requisição de listagem de cursos

--- src/main/java/com/example/CouseAPI/modules/course/controllers/CourseController.java
+++ src/main/java/com/example/CouseAPI/modules/course/controllers/CourseController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cursos")
@@ -23,6 +20,18 @@ public class CourseController {
         try{
             var result = this.courseService.createCourse(courseEntity);
             return ResponseEntity.status(HttpStatus.CREATED).body(result);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAll(){
+        try {
+
+            var result = this.courseService.getCourses();
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+
         }catch (Exception e){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
--- src/main/java/com/example/CouseAPI/modules/course/services/CourseService.java
+++ src/main/java/com/example/CouseAPI/modules/course/services/CourseService.java
@@ -6,6 +6,8 @@ import com.example.CouseAPI.modules.course.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CourseService {
     @Autowired
@@ -20,6 +22,11 @@ public class CourseService {
                 });
 
         return this.courseRepository.save(courseEntity);
+    }
+
+    public List<CourseEntity> getCourses(){
+        var courses = this.courseRepository.findAll();
+        return courses;
     }
 
 }
